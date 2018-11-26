5403df911fdb98951f0d5b20177efa2f2972c2f8
hakan akyürek <akyurekhakanarbeit@gmail.com>
2018-11-26 12:15:19 +0300

    Script written for colliders in hand.

---
+++ VR-hand-hygiene/Assets/Scripts/HandCollider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Obi;
+public class HandCollider : MonoBehaviour {
+
+    public Transform parent;
+
+	void Start () {
+
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            Transform notend = parent.GetChild(i);
+            for (int j= 0;j < notend.childCount; j++)
+            {
+                Transform a = notend.GetChild(j);
+                BoxCollider col = a.gameObject.AddComponent<BoxCollider>();
+                col.size = new Vector3(.02f,.02f,.02f);
+               
+                ObiCollider obiCollider = a.gameObject.AddComponent<ObiCollider>();
+                obiCollider.SourceCollider = col;
+            }
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
