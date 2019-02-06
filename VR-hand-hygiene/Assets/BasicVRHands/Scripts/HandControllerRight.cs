d49ed518c5b7c62a697f2aa6c0cd593b109ecf09
hakan akyürek <akyurekhakanarbeit@gmail.com>
2019-02-06 13:19:46 +0300

    New hand model assets added, will clean.

---
+++ VR-hand-hygiene/Assets/BasicVRHands/Scripts/HandControllerRight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR.InteractionSystem;
+public class HandControllerRight : MonoBehaviour {
+
+    private Animator animator;
+	public Hand hand; 
+	void Start () {
+	
+		hand = GameObject.Find("Hand1").GetComponent<Hand>();	
+        animator = GetComponent<Animator>();
+		transform.SetPositionAndRotation(gameObject.transform.position, Quaternion.Euler(0.0f ,0.0f ,-70.0f));
+	
+	}
+	
+	void Update () {
+        /*
+		if(hand.objectTag == "Small")
+			animator.SetBool("isGrabbing", hand.controller.GetPress(SteamVR_Controller.ButtonMask.Trigger));
+		else if(hand.objectTag == "Big")
+			animator.SetBool("isGrabbingBig", hand.controller.GetPress(SteamVR_Controller.ButtonMask.Trigger));
+		else if(hand.objectTag == "Middle")
+			animator.SetBool("isGrabbingMid", hand.controller.GetPress(SteamVR_Controller.ButtonMask.Trigger));
+		else
+			animator.SetBool("isGrabbingMid", hand.controller.GetPress(SteamVR_Controller.ButtonMask.Trigger));
+	    */
+	}
+}
